3d46eac44570e36c819001d0435960e978689e33
jonesmoshekofficial@gmail.com <jonesmoshekofficial@gmail.com>
2026-01-26 07:53:44 +0530

    Three different files are added

---
+++ pat2.java
@@ -0,0 +1,21 @@
+//Same number rows
+
+public class pat2 {
+    public static void main(String[] args) {
+        int i,j,n=5;
+        for ( i = 1; i <= n; i++) {
+            for(j = 1;j<=n;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
+
+//Output:
+/*
+1 1 1 1 1  
+2 2 2 2 2  
+3 3 3 3 3  
+4 4 4 4 4  
+5 5 5 5 5 */
---
+++ pat3.java
@@ -0,0 +1,22 @@
+//Same number rows
+
+public class pat3 {
+    public static void main(String[] args) {
+        int i,j,n=5;
+        for ( i = 1; i <= n; i++) {
+            for(j = 1;j<=n;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
+
+//Output:
+/*
+1 2 3 4 5  
+1 2 3 4 5  
+1 2 3 4 5  
+1 2 3 4 5  
+1 2 3 4 5 
+ */
