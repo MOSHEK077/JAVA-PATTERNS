ba58a4d4429a1ca6192bc96383f63c0a3163a023
jonesmoshekofficial@gmail.com <jonesmoshekofficial@gmail.com>
2026-01-26 07:41:17 +0530

    Adding first file

---
+++ pat1.java
@@ -0,0 +1,23 @@
+//Square pattern 
+class pat1{
+    public static void main(String[] args) {
+        int i,j,n = 5;
+        for ( i = 1; i <= n; i++) {
+            for ( j = 1; j <= n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
+
+//output:
+/*
+
+* * * * *  
+* * * * *  
+* * * * *  
+* * * * *  
+* * * * *  
+
+ */
