7b549d2eaca0b8bcd6fb5ea4f161dfbfaba418cb
jonesmoshekofficial@gmail.com <jonesmoshekofficial@gmail.com>
2026-01-27 19:06:51 +0530

    three files are added

---
+++ pat5.java
@@ -0,0 +1,30 @@
+//Hollow square numbers
+
+class pat5 {
+    public static void main(String[] args) {
+        int i , j , n= 5;
+        for ( i = 1; i <= n ; i++) {
+            for ( j = 1; j <= n; j++) {
+                if(i ==1|| i == n|| j == 1|| j == n){
+                    System.out.print(i+" ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+               
+            }
+             System.out.println();
+        }
+    }
+    
+}
+/*
+
+1 1 1 1 1 
+2       2
+3       3
+4       4
+5 5 5 5 5
+
+
+ */
--- pat6.java
+++ pat6.java
@@ -1,10 +1,12 @@
-class pat4{
+//Border numbers
+
+class pat6 {
     public static void main(String[] args) {
         int i , j , n = 5;
         for(i = 1; i <= n ; i++){
             for(j = 1; j <= n ; j++){
-                if(i == 1 || i == n || j == 1|| j ==n){
-                    System.out.print("* ");
+                if(i ==1 || i == n || j == 1|| j==n){
+                    System.out.print(j+" ");
                 }
                 else{
                     System.out.print("  ");
