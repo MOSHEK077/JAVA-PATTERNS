93dc0afe2d19f3f1d2f52ce7b0394c0c6ce43f9c
jonesmoshekofficial@gmail.com <jonesmoshekofficial@gmail.com>
2026-01-29 14:26:32 +0530

    might changes

--- pat5.java
+++ pat5.java
@@ -1,30 +1,20 @@
-//Hollow square numbers
 
-class pat5 {
+//Hallow square numbers
+class pat5{
     public static void main(String[] args) {
-        int i , j , n= 5;
-        for ( i = 1; i <= n ; i++) {
+        int i , j , n = 5;
+        for ( i = 1; i <= n; i++) {
             for ( j = 1; j <= n; j++) {
-                if(i ==1|| i == n|| j == 1|| j == n){
+                if(i == 1 || i == n || j == 1 || j ==n){
                     System.out.print(i+" ");
                 }
                 else{
                     System.out.print("  ");
                 }
-               
+                
             }
-             System.out.println();
+            System.out.println();
+            
         }
     }
-    
 }
-/*
-
-1 1 1 1 1 
-2       2
-3       3
-4       4
-5 5 5 5 5
-
-
- */
