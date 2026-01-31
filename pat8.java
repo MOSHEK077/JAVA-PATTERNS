9e78bbc8c707bbd18b6d71240798986d7efe8a67
jonesmoshekofficial@gmail.com <jonesmoshekofficial@gmail.com>
2026-01-31 15:36:26 +0530

    Incremental Numbers is added

--- pat8.java
+++ pat8.java
@@ -1,19 +1,24 @@
+//Multiplication Table Pattern
+
 class pat8{
     public static void main(String[] args) {
-        int i , j , n = 10;
-        for(i = 1; i<= n;i++){
-            for(j = 1; j <= n ; j++){
-                if(i * j < 10)
+        int i ,j , n  =10 ;
+        for(i = 1; i <= n ; i++){
+            for(j = 1; j <= n ; j ++){
+                if(i*j< 10)
                     System.out.print("0"+(i*j)+" ");
                 else
                     System.out.print(i*j+" ");
+                
             }
-            System.out.println();
+                    System.out.println();
+
+            
         }
     }
 }
 
-//Output:
+
 /*
 
 01 02 03 04 05 06 07 08 09 10 
@@ -26,6 +31,5 @@ class pat8{
 08 16 24 32 40 48 56 64 72 80 
 09 18 27 36 45 54 63 72 81 90 
 10 20 30 40 50 60 70 80 90 100 
-
 
 */
---
+++ pat9.java
@@ -0,0 +1,21 @@
+//Incremental Numbers;
+class pat9{
+    public static void main(String[] args) {
+        int i , j , n = 5;
+        for(i = 1; i<= n ;i++){
+            for(j = 1; j<= n ;j++){
+                System.out.print(i+j-1+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+//Output
+/*
+1 2 3 4 5 
+2 3 4 5 6 
+3 4 5 6 7 
+4 5 6 7 8 
+5 6 7 8 9 
+ */
