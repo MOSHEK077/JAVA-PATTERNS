142e00259bc445829a1ceb612d63583799020075
jonesmoshekofficial@gmail.com <jonesmoshekofficial@gmail.com>
2026-02-07 21:09:16 +0530

    new file added

---
+++ pat15.java
@@ -0,0 +1,14 @@
+class pat15{
+    public static void main(String[] args) {
+        int i , j , k , n=  5;
+        for(i = 1; i <= n ; i++){
+            for(j = 1; j <= n - i ; j++){
+                System.out.print(" ");
+            }
+            for(k = 1; k <= i; k++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
