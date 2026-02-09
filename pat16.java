310fa4aa76bd0d1ed85a5d8cc1cc2003fdfde228
jonesmoshekofficial@gmail.com <jonesmoshekofficial@gmail.com>
2026-02-09 14:45:13 +0530

    new file added

---
+++ pat16.java
@@ -0,0 +1,26 @@
+class pat16{
+    public static void main(String[] args) {
+        int i , j , k , n = 5;
+        for(i = 1; i <= n;i++){
+            for(k = 1; k<= n-i;k++){
+                System.out.print(" ");
+            }
+            for(j = 1; j<= i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+//Output:
+/*
+//Increment numbers triangle
+    1 
+   1 2 
+  1 2 3 
+ 1 2 3 4 
+1 2 3 4 5 
+
+
+ */
