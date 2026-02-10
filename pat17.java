82dff6109de3b120b472479ac1692ff18e39e229
jonesmoshekofficial@gmail.com <jonesmoshekofficial@gmail.com>
2026-02-10 15:12:46 +0530

    Final Exercise Added

---
+++ pat17.java
@@ -0,0 +1,25 @@
+public class pat17 {
+
+    public static void main(String[] args) {
+        int i , j , k , n = 5;
+        for(i = 1; i <= n ; i++){
+            for(k = 1; k <= n - i; k++){
+                System.out.print(" ");
+            }
+            for(j = 1; j <= i ; j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+//Output:
+/*
+Centered Star Prymid
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * * 
+*/
---
+++ pat18.java
@@ -0,0 +1,28 @@
+class pat18{
+    public static void main(String[] args) {
+        int i , j , k , n = 5;
+        for(i = 1; i <= n  ; i++){
+            for(k = 1; k <= n - i ; k++){
+                System.out.print(" ");
+            }
+            for(j = 1; j <= i ; j++){
+                if(i == n || j == 1|| j == i)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+//Output:
+/*
+
+    * 
+   * * 
+  *   * 
+ *     * 
+* * * * * 
+
+*/
